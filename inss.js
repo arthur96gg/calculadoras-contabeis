74a1d1fb0551c45c04e23de9a26b3aff24691c33
sophiacouto <sophiacouto.rolo@gmail.com>
2026-05-15 12:11:14 -0300

    fizemos 12%

--- inss.js
+++ inss.js
@@ -1,9 +1,12 @@
-const salariobruto = 2800
+const salariobruto = 3800
 let inss = 0
 if (salariobruto <= 1621){
     inss = salariobruto * 0.075
 }else if(salariobruto > 1621 && salariobruto <=2902.84){
      inss = (salariobruto - 1621)*0.09 +121.58
+}else if(salariobruto > 2902.84 && salariobruto <= 4354.27){
+   inss = (salariobruto - 2902.84)*0.12 + 115.37 + 121.58
 }
 
-console.log(inss)
+console.log(inss.toFixed(2))
+
